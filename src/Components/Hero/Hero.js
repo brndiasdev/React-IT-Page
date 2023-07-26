5a614caf3edede73e24d50f1af973b7e2e7d0808
Bruno Dias <62982430+brndias1@users.noreply.github.com>
2023-07-26 18:43:48 -0300

    additional changes

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -2,32 +2,39 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Button, Container, MainHeading } from "../../globalStyles";
 import {
-  HeroVideo,
-  HeroSection,
-  HeroText,
-  ButtonWrapper,
-  HeroButton,
+	HeroVideo,
+	HeroSection,
+	HeroText,
+	ButtonWrapper,
+	HeroButton,
 } from "./HeroStyles";
 import cover from "./hero.mp4";
 
 const Hero = () => {
-  return (
-    <HeroSection>
-      <HeroVideo src={cover} autoPlay loop muted />
-      <Container>
-        <MainHeading>Your Data is secure with us</MainHeading>
-        <HeroText>
-          We Provide the best security systems for clients all over the world
-        </HeroText>
-        <ButtonWrapper>
-          <Link to="signup">
-            <Button>Get Started</Button>
-          </Link>
-          <HeroButton>Find More</HeroButton>
-        </ButtonWrapper>
-      </Container>
-    </HeroSection>
-  );
+	return (
+		<HeroSection>
+			<HeroVideo
+				src={cover}
+				autoPlay
+				loop
+				muted
+			/>
+			<Container>
+				<MainHeading>Your Data is secure with us</MainHeading>
+				<HeroText>
+					We Provide the best security systems for clients all over the world
+				</HeroText>
+				<ButtonWrapper>
+					<Link to='signup'>
+						<Button>Get Started</Button>
+					</Link>
+					<Link to='pricing'>
+						<HeroButton>Find More</HeroButton>
+					</Link>
+				</ButtonWrapper>
+			</Container>
+		</HeroSection>
+	);
 };
 
 export default Hero;
